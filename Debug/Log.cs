7ae4061914be7a99193651ba7ad52467f893d11d
monge <ekioprogram@gmail.com>
2020-08-10 01:10:56 +0900

    Logシステム追加

---
+++ Debug/Log.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Ashe
+{
+    namespace Debug
+    {
+        public class Log
+        {
+            [Conditional("_LOG_ENABLED")]
+            public static void I (string message)
+            {
+                UnityEngine.Debug.Log(message);
+            }
+
+            [Conditional("_LOG_ENABLED")]
+            public static void W(string message)
+            {
+                UnityEngine.Debug.Log(message);
+            }
+
+            [Conditional("_LOG_ENABLED")]
+            public static void E(string message)
+            {
+                UnityEngine.Debug.Log(message);
+            }
+
+#if _LOG_ENABLED
+            class LogInfo
+            {
+                public enum TYPE
+                {
+                    INFO,
+                    WARNING,
+                    ERROR,
+                }
+                public DateTime time;
+                public TYPE type;
+                public string message;
+            }
+            List<LogInfo> logInfos = new List<LogInfo>();
+#endif
+        }
+
+    }
+}
