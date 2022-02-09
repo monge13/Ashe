216f6fec9c7081a09c6826cadd452f1bf90d7bcc
monge13 <ekioprogram@gmail.com>
2022-02-09 10:24:27 +0900

    added binary serializer

---
+++ Util/BinarySerializer.cs
@@ -0,0 +1,32 @@
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Ashe
+{
+    namespace Util
+    {
+        public class BinarySerializer
+        {
+
+            public static void Seialize<T>(string path, T obj)
+            {
+                using (FileStream fs = new FileStream(path, FileMode.Create, FileAccess.Write))
+                {
+                    BinaryFormatter bf = new BinaryFormatter();
+                    bf.Serialize(fs, obj);
+                }
+            }
+
+            public static T Deserialize<T>(string path)
+            {
+                T obj;
+                using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+                {
+                    BinaryFormatter f = new BinaryFormatter();
+                    obj = (T)f.Deserialize(fs);
+                }
+                return obj;
+            }
+        }
+    }
+}
