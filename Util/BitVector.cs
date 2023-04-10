85b7fbac3f49332a634af42db7ad04ce6a4a5fe2
monge13 <ekioprogram@gmail.com>
2023-04-10 18:11:42 +0900

    boolのVectorを作成

---
+++ Util/BitVector.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ashe
+{
+    namespace Util
+    {
+        public struct BitVector3
+        {
+            public bool x,y,z;
+        }
+
+        public struct BitVector4
+        {
+            public bool x,y,z,w;
+        }
+    }
+}
