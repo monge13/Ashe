3949ee13bdb6d22702c15d359c91734112e04f29
monge13 <ekioprogram@gmail.com>
2023-11-13 16:08:57 +0900

    Transformのパスを取得する拡張

---
+++ Editor/HierarchyMenu.cs
@@ -0,0 +1,31 @@
+using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// ヒエラルキービューに右クリックメニューを追加
+/// </summary>
+public class HierarchyMenu {
+
+    [MenuItem("Edit/CopyTransformName", false, 0)]
+    public static void CopyTransformName()
+    {
+        var gameObject = Selection.activeGameObject;
+        GUIUtility.systemCopyBuffer = GetPath(gameObject.transform);
+    }
+
+    public static string GetPath(Transform self)
+    {
+        string path = self.gameObject.name;
+        var root = PrefabUtility.GetOutermostPrefabInstanceRoot(self);
+
+        Transform parent = self.parent;
+
+        while (parent != null)
+        {
+            path = parent.name + "/" + path;            
+            parent = parent.parent;            
+            if(parent.gameObject == root.gameObject) break; 
+        }    
+        return path;
+    }
+}
