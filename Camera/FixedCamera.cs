aececf3bb80120eb6299b28273188b8a280405f8
monge13 <ekioprogram@gmail.com>
2024-01-29 17:03:21 +0900

    固定カメラの作成

---
+++ Camera/FixedCamera.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ashe
+{
+    public class FixedCamera : CameraBase
+    {
+        // 追従する対象のTransform
+        [SerializeField]
+        Transform _followObject;
+ 
+        [SerializeField]
+        Vector3 _offset;
+ 
+                // カメラの更新 
+        void LateUpdate()
+        {
+            _cachedTransform.SetPositionAndRotation(
+                _followObject.position + _offset,
+                _cachedTransform.rotation
+            );
+            UpdateTransform();
+        }
+
+    }
+}
